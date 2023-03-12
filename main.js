2023919c4f2d5a98cd48b62667633c467f80bdac
Thameem-20 <119170093+Thameem-20@users.noreply.github.com>
2023-03-12 13:18:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form');
+const result = document.querySelector('#result');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+    var totalClasses=document.getElementById('classes').value;
+	var attendedClasses = document.getElementById('attended').value;
+    totalClasses =Number(totalClasses);
+    attendedClasses =Number(attendedClasses);
+
+	var attendancePercentage = (attendedClasses / totalClasses) * 100;
+    document.getElementById('percentage').innerHTML=attendancePercentage +"%";
+
+	var classesNeeded = Math.ceil((0.85 * totalClasses - attendedClasses) );
+    if(classesNeeded<0){
+        classesNeeded=0;
+    }
+    document.getElementById('result').innerHTML= classesNeeded;
+    if(attendancePercentage < 85 && attendancePercentage>=80 ){
+        document.getElementById('fine').innerHTML="100 ";
+    }else if (attendancePercentage <80 && attendancePercentage >=75 ) {
+        document.getElementById('fine').innerHTML="500";
+    } else if(attendancePercentage <75 && attendancePercentage>=70) {
+        document.getElementById('fine').innerHTML="1000";
+        
+    }else if(attendancePercentage <70) {
+        document.getElementById('fine').innerHTML=" above 1000";
+    }
+});
+
+function popup(){
+    document.getElementById('popup').style.display="block";
+}
+function hidde(){
+    document.getElementById('popup').style.display="none";
+}
